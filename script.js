958cf81e43c3d880230dad187cebd2da8adb9c24
gRAnULa2771 <100364130+gRAnULa2771@users.noreply.github.com>
2022-02-24 18:17:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"use script"
+<select onchange="window.location.href = this.options[this.selectedIndex].value">
+	<option value=""></option>
+	<option value="https://example.com/?brand=1">Peronda</option>
+	<option value="https://example.com/?brand=2">Porcelanite Dos</option>
+	<option value="https://example.com/?brand=3">Piemme Valentino</option>
+	<option value="https://example.com/?brand=4">Dune</option>
+	<option value="https://example.com/?brand=5">Atlas Concorde</option>
+	<option value="https://example.com/?brand=6">Colorker</option>
+	<option value="https://example.com/?brand=7">Golden Tile</option>
+	<option value="https://example.com/?brand=8">Grespania</option>
+	<option value="https://example.com/?brand=9">Vallelunga</option>
+	<option value="https://example.com/?brand=10">Fanal</option>
+</select>
